4770c8295bf376346536a201f9575876f6f50a53
Samuel Barney <samuel13@uga.edu>
2016-03-01 16:28:58 +0000

    Add categories model and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301131147) do
+ActiveRecord::Schema.define(version: 20160301142409) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20160301131147) do
     t.datetime "updated_at"
     t.integer  "user_ud"
     t.integer  "user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: :cascade do |t|
