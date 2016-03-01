acd7decd0c384caf5946a8a336be965709976713
Samuel Barney <samuel13@uga.edu>
2016-03-01 19:17:05 +0000

    Update all views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,6 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    #debugger
     @article = Article.new(article_params)
     @article.user = current_user
     if @article.save
@@ -50,7 +49,7 @@ class ArticlesController < ApplicationController
     end
 
     def article_params
-      params.require(:article).permit(:title, :description)
+      params.require(:article).permit(:title, :description, category_ids: [])
     end
 
   def require_same_user
